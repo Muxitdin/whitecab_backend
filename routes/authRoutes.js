465947ab4f94618d99ca8b463bfaca70d6c45e86
Muxitdin <toraananas@gmail.com>
2024-09-11 16:17:29 +0500

    fixed a bug
    corrected the auth routehandler's HTTP-method

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { getAuth, register, login } from "../controllers/authControllers.js"
+import authentication from "../middlewares/authentication.js"
 const router = express.Router();
 
-router.post('/', getAuth)
+router.get('/', authentication, getAuth)
 router.post('/register', register)
 router.post('/login', login)
 
