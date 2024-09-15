c39bfddb2ebaa66d638f79ad53317bd41cc115a5
Muxitdin <toraananas@gmail.com>
2024-09-15 19:16:04 +0500

    switched to gmail

--- config/mailOptions.js
+++ config/mailOptions.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 module.exports = function (mailTitle, mailText) {
     const mailOptions = {
         from: process.env.EMAIL,
--- config/transporter.js
+++ config/transporter.js
@@ -4,9 +4,10 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 module.exports = nodemailer.createTransport({
-    host: 'smtp.yandex.com',
-    port: 465,
-    secure: true,
+    // host: 'smtp.yandex.com',
+    // port: 465,
+    // secure: true,
+    service: 'gmail',
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASS
