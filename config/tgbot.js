5c8fd00eae3d72258b3a714c2726c7ef7d5924a2
Muxitdin <toraananas@gmail.com>
2024-09-17 21:23:46 +0500

    implement rate limit handling for webhook requests

--- config/tgbot.js
+++ config/tgbot.js
@@ -1,3 +1,4 @@
+const Bottleneck = require('bottleneck');
 const TelegramBot = require('node-telegram-bot-api');
 const token = '6545177490:AAEA9VD7RMAGNhDA0khZ6cJ2FwC8HQENvbg';
 const bot = new TelegramBot(token);
@@ -5,12 +6,28 @@ const chatId = '2144613960';
 
 bot.setWebHook(`${process.env.SERVER_URL}/api/telegram/bot-path`);
 
+const limiter = new Bottleneck({
+    minTime: 1000  // Минимальное время между запросами (1 секунда)
+});
+
+const sendMessage = limiter.wrap((chatId, message) => {
+    return bot.sendMessage(chatId, message);
+});
+
 module.exports = async function sendRegistrationData(userData) {
   const message = `Новый пользователь зарегистрирован:\n\nИмя: ${userData.name}\nТелефон: +${userData.phone}`;
   
-  bot.sendMessage(chatId, message)
+  sendMessage(chatId, message)
     .then(() => console.log('Сообщение отправлено'))
-    .catch(error => console.error('Ошибка отправки сообщения', error));
+    .catch(error => {
+        if (error.response && error.response.body && error.response.body.parameters) {
+            const retryAfter = error.response.body.parameters.retry_after;
+            console.log(`Повторная попытка через ${retryAfter} секунд`);
+            setTimeout(() => sendMessage(chatId, message), retryAfter * 1000);
+        } else {
+            console.error('Ошибка отправки сообщения', error);
+        }
+    });
 }
 
 module.exports = bot;
